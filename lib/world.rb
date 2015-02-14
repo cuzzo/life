2f98ad8421d10a87867a72d05a5e49a5b3f1a6fc
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2015-02-14 12:48:22 +0200

    Retrieve cell via x/y

--- lib/world.rb
+++ lib/world.rb
@@ -4,13 +4,19 @@ class World
   def initialize(width, height)
     @cells = []
     height.times do |x|
+      @cells << []
       width.times do |y|
-        @cells << Cell.new
+        cell = Cell.new
+        @cells[x] << cell
       end
     end
   end
 
   def cells
-    @cells
+    @cells.flatten
+  end
+
+  def cell_at(x, y)
+    @cells[x][y] if @cells[x]
   end
 end
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -9,4 +9,22 @@ describe World do
     it { is_expected.to have_exactly(36).cells }
     it { is_expected.to all be_dead }
   end
+
+  describe '#cell_at' do
+    subject { world.cell_at(x, y) }
+
+    context 'sixth cell' do
+      let(:x) { 0 }
+      let(:y) { 5 }
+
+      it { is_expected.to eq world.cells[5] }
+    end
+
+    context 'seventh cell' do
+      let(:x) { 1 }
+      let(:y) { 0 }
+
+      it { is_expected.to eq world.cells[6] }
+    end
+  end
 end
