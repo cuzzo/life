1f08ee87e52d671e3d160a22b16f603d3d9bd558
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2015-02-19 21:35:58 +0200

    Refactor specs for first two rules to reduce duplication

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -33,38 +33,35 @@ describe World do
       world.next_generation!
       cell
     end
+
     let(:cell) { world.cell_at(0, 0) }
+    let(:neighbours) { cell.neighbours.first(neighbour_count) }
 
-    context 'live cell with no live neighbours' do
-      before { cell.toggle }
-      it { is_expected.to be_dead }
-    end
-
-    context 'live cell with one live neighbour' do
-      let(:neighbour) { cell.neighbours.first }
-      before do
-        cell.toggle
-        neighbour.toggle
-      end
-      it { is_expected.to be_dead }
-    end
-
-    context 'live cell with two live neighbours' do
-      let(:neighbours) { cell.neighbours.first(2) }
+    context 'live cell' do
       before do
         cell.toggle
         neighbours.each(&:toggle)
       end
-      it { is_expected.to be_alive }
-    end
 
-    context 'live cell with three live neighbours' do
-      let(:neighbours) { cell.neighbours.first(3) }
-      before do
-        cell.toggle
-        neighbours.each(&:toggle)
+      context 'with no live neighbours' do
+        let(:neighbour_count) { 0 }
+        it { is_expected.to be_dead }
       end
-      it { is_expected.to be_alive }
+
+      context 'with one live neighbour' do
+        let(:neighbour_count) { 1 }
+        it { is_expected.to be_dead }
+      end
+
+      context 'with two live neighbours' do
+        let(:neighbour_count) { 2 }
+        it { is_expected.to be_alive }
+      end
+
+      context 'with three live neighbours' do
+        let(:neighbour_count) { 3 }
+        it { is_expected.to be_alive }
+      end
     end
   end
 end
